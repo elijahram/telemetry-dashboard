8b4c4d7cd316bc9c936a385f1a2d2aa1087f2978
Elijah <elijahbramos@gmail.com>
2026-08-12 22:00:57 -0700

    feat: added anomaly and telemetry components to dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,55 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
-import { API_BASE_URL } from "./api/config";
-import SummaryCard from "./components/Summary/SummaryCard";
-import type { SensorSummary } from "./types/telemetry";
+import SummaryCard from "./components/SensorSummary/SummaryCard";
+import AnomalyTable from "./components/Anomaly/AnomalyTable";
+import TelemetryChart from "./components/TelemetryData/TelemetryChart";
+import type {
+  AnomalyRow,
+  SensorSummary,
+  TelemetryRow,
+} from "./types/telemetry";
+import {
+  fetchAnomalies,
+  fetchSensorSummary,
+  fetchTelemetryData,
+} from "./api/telemetry";
 
 const App = () => {
   const [summaries, setSummaries] = useState<SensorSummary[]>([]);
+  const [anomalies, setAnomalies] = useState<AnomalyRow[]>([]);
+  const [telemetryData, setTelemetryData] = useState<TelemetryRow[]>([]);
 
   useEffect(() => {
-    axios
-      .get(`${API_BASE_URL}/sensor_summary`)
-      .then((res) => setSummaries(res.data))
-      .catch((err) => console.log(err));
+    fetchSensorSummary()
+      .then((data) => setSummaries(data))
+      .catch((err) => console.error("Error fetching summaries:", err));
+    fetchAnomalies()
+      .then((data) => setAnomalies(data))
+      .catch((err) => console.error("Error fetching anomalies:", err));
+    fetchTelemetryData()
+      .then((data) => setTelemetryData(data))
+      .catch((err) => console.error("Error fetching telemetry data:", err));
   }, []);
 
   return (
     <div>
       <h1>Telemetry Dashboard</h1>
-      {summaries.map((summary) => (
-        <SummaryCard key={summary.sensor} data={summary} />
-      ))}
+      <div>
+        {summaries.map((summary) => (
+          <SummaryCard key={summary.sensor} data={summary} />
+        ))}
+      </div>
+      <div>
+        <h1>Anomalies</h1>
+        {anomalies.map((anomaly) => (
+          <AnomalyTable key={anomaly.sensor} data={anomaly} />
+        ))}
+      </div>
+      <div>
+        <h1>Telemetry Data</h1>
+        {telemetryData.map((data) => (
+          <TelemetryChart key={data.timestamp} data={data} />
+        ))}
+      </div>
     </div>
   );
 };
